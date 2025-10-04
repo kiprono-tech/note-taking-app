bbf94e9e060b608d7e585032e8b824b94ffecf6a
kiprono-tech <kiprono.tech@gmail.com>
2025-10-04 18:12:14 +0300

    Initial commit - notes app setup

--- app.js
+++ app.js
@@ -1,15 +1,13 @@
-import yargs from 'yargs';
-import { hideBin } from 'yargs/helpers';
-import chalk from 'chalk';
-import getNotes from './notes.js';
+const yargs = require('yargs');
+const { hideBin } = require('yargs/helpers');
+const chalk = require('chalk');
+const notes = require('./notes.js');
 
 // Create yargs parser instance
 const y = yargs(hideBin(process.argv));
 
-// customize yargs version
 y.version('1.1.0');
 
-// create add command
 y.command({
     command: 'add',
     describe: 'Add a new note',
@@ -26,26 +24,8 @@ y.command({
         }
     },
     handler(argv) {
-        console.log('Title: ' + argv.title);
-        console.log('Body: ' + argv.body);
+        notes.addNote(argv.title, argv.body);
     }
 });
 
-// create remove command
-// y.command({
-//     command: 'remove',
-//     describe: 'Remove a note',
-//     builder: {
-//         title: {
-//             describe: 'Note title',
-//             demandOption: true,
-//             type: 'string'
-//         }
-//     },
-//     handler(argv) {
-//         console.log('Removing note: ' + argv.title);
-//     }
-// });
-
-// Parse arguments
 y.parse();
